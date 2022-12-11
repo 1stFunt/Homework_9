98f8091f6f122ac2f016df3b5c8be66a16bb1bdc
Funt <funt_88@mail.ru>
2022-12-12 02:28:12 +0300

    all completed

---
+++ Task_1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+int number = ReadInt("Введите натуральное число N: ");
+Console.Write(NaturalNumber(number));
+int NaturalNumber(int n)
+{
+    if (n == 1)
+        return n;
+    else
+    {
+        Console.Write(n + ", ");
+        NaturalNumber(n - 1);
+    }
+    return 1;
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
---
+++ Task_3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+int m = ReadInt("Введите положительное число M: ");
+int n = ReadInt("Введите положительное число N: ");
+Console.WriteLine($"A({m}, {n}) = {Akkerman(m, n)}");
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    if (m > 0 && n == 0)
+        return Akkerman(m - 1, 1);
+    else
+        return Akkerman(m - 1, Akkerman(m, n - 1));
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
